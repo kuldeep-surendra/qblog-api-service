b35e138993faffae01e88fdf32a0614739d88bb6
kuldeep-surendra <ksurendra@qwinix.io>
2018-06-14 17:55:04 +0530

    changes with auth0

--- app.js
+++ app.js
@@ -1,75 +1,10 @@
-// const Hapi = require('hapi');
-// const jwt = require('hapi-auth-jwt2');
-// const jwksRsa = require('jwks-rsa');
-// const logger = require('debug')('hapi');
-
-// const routes = require('./config/routes');
-
-
-// const validate = async (decoded, request, callback) => { 
-//   logger('Validating user:', decoded);
-
-//   if (decoded && decoded.sub) {
-//     return callback(null, true);
-//   }
-
-//   return callback(null, false);
-// }
-// const init = async () => { 
-  
-//   const server =  new Hapi.Server({ debug: { log: [ 'error' ] } });
-//    server.connection({
-//     host: "0.0.0.0",
-//     port: 3001
-//   });
-
-//   server.register(jwt, (err) => {
-//     if (err) {
-//       console.error('Failed to load a plugin:', err);
-//     }
-
-//     server.auth.strategy('jwt', 'jwt','required', { 
-//       complete: true,
-//       key: jwksRsa.hapiJwt2Key({
-//         cache: true,
-//         rateLimit: true,
-//         jwksRequestsPerMinute: 5,
-//         jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-//       }),
-//       verifyOptions: { 
-//         audience: process.env.AUTH0_AUDIENCE,
-//         issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-//         algorithms: ['RS256']
-//       },
-//       validateFunc: validate
-//     });
-  
-//     // server.auth.default('jwt');
-//     // server.route(routes);
-//       server.route({
-//     method: 'GET',
-//     path: '/api/private',
-//     config: {
-//       auth: 'jwt',
-//       cors: true,
-//       handler: (req, res) => {
-//         res({
-//           message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-//         });
-//       }
-//     }
-//   });
-//   })
-//   server.start();
-//   console.log(`Server running at: ${server.info.uri}`);
-// }
-// init();
 'use strict';
 
 const Hapi = require('hapi');
 const jwt = require('hapi-auth-jwt2');
 const jwksRsa = require('jwks-rsa');
 require('dotenv').config();
+const routes = require('./config/routes');
 
 const server = new Hapi.Server();
 
@@ -83,50 +18,51 @@ server.connection({
   }
 });
 
-const registerRoutes = () => {
-  server.route({
-    method: 'GET',
-    path: '/api/public',
-    config: {
-      auth: false,
-      handler: (req, res) => {
-        res({
-          message: "Hello from a public endpoint! You don't need to be authenticated to see this."
-        });
-      }
-    }
-  });
+// const registerRoutes = () => {
+//   server.route({
+//     method: 'GET',
+//     path: '/api/public',
+//     config: {
+//       auth: false,
+//       cors: corsHeader,
+//       handler: (req, res) => {
+//         res({
+//           message: "Hello from a public endpoint! You don't need to be authenticated to see this."
+//         });
+//       }
+//     }
+//   });
 
-  server.route({
-    method: 'GET',
-    path: '/api/private',
-    config: {
-      auth: 'jwt',
-      cors: true,
-      handler: (req, res) => {
-        res({
-          message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-        });
-      }
-    }
-  });
+//   server.route({
+//     method: 'GET',
+//     path: '/api/private',
+//     config: {
+//       auth: 'jwt',
+//       cors: corsHeader,
+//       handler: (req, res) => {
+//         res({
+//           message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+//         });
+//       }
+//     }
+//   });
 
-  server.route({
-    method: 'GET',
-    path: '/api/private-scoped',
-    config: {
-      auth: {
-        scope: 'read:messages'
-      },
-      cors: true,
-      handler: (req, res) => {
-        res({
-          message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-        });
-      }
-    }
-  });
-};
+//   server.route({
+//     method: 'GET',
+//     path: '/api/private-scoped',
+//     config: {
+//       auth: {
+//         scope: 'read:messages'
+//       },
+//       cors: corsHeader,
+//       handler: (req, res) => {
+//         res({
+//           message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
+//         });
+//       }
+//     }
+//   });
+// };
 
 const validateUser = (decoded, request, callback) => {
   // This is a simple check that the `sub` claim
@@ -163,10 +99,11 @@ server.register(jwt, err => {
     },
     validateFunc: validateUser
   });
-  registerRoutes();
+  // registerRoutes();
+  server.route(routes);
 });
 
 server.start(err => {
   if (err) throw err;
-  console.info(`Server started at http://localhost:3010`);
+  console.info(`Server running at: ${server.info.uri}`);
 });
--- config/routes.js
+++ config/routes.js
@@ -1,28 +1,22 @@
+
+const corsHeader = {
+  origin: ['*'],
+  headers: ['Access-Control-Allow-Origin', 'Access-Control-Allow-Headers', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'AuthKey', 'Authorization', 'Email'],
+  credentials: true
+};
+
 const welcomeRoute = [{
-  // method: "GET",
-  // path: "/api/private",
-  // config: { auth: 'jwt', cors: true},
-  // handler: (req, res) => {
-  //   res({
-  //         message: "Hello from a public endpoint! You don't need to be authenticated to see this."
-  //       });
-  // }
-
-
-
-
   method: 'GET',
-      path: '/api/private',
-      config: {
-        auth: 'jwt',
-        cors: true,
-        handler: (req, res) => {
-          console.log(res)
-          res({
-            message: "Hello from a public endpoint! You don't need to be authenticated to see this."
-          });
-        }
+    path: '/api/private',
+    config: {
+      auth: 'jwt',
+      cors: corsHeader,
+      handler: (req, res) => {
+        res({
+          message: 'Hello from a private endpoint! You need to be authenticated to see this.'
+        });
       }
+    }
 }];
 
 module.exports = [].concat(welcomeRoute);
